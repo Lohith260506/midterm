7e322d92fdb89ce0e133b8875a6fba862518e501
Lohith260506 <lohith.s2023@vitstudent.ac.in>
2026-04-09 17:07:13 +0530

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -6,7 +6,6 @@ public class AppTest {
     @Test
     public void testTransmission() throws Exception {
         App app = new App();
-        // Use a high port number to avoid system conflicts
         assertNotNull(app.transmit("Test Message", 9999));
     }
 }
